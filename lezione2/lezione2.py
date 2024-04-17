a2806fc577cf71c4211b9f92628a126b272d58ec
DaniFior <fioravantidaniele@icloud.com>
2024-04-17 13:49:10 +0200

    File lezione2.py aggiunto

---
+++ lezione2/lezione2.py
@@ -0,0 +1,9 @@
+# Daniele Fioravanti
+# 17/04/2024
+
+print("Hello world!") 
+
+
+name: str = "Mario"
+message: str = "Hello {} do you want otlearn some python together?".format(name)
+print (message)
