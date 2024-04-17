ddd83c8a871df6482844b9739c22618640f1a10c
DaniFior <fioravantidaniele@icloud.com>
2024-04-17 13:49:44 +0200

    Upload lezione2.py

---
+++ lezione1/lezione2.py
@@ -0,0 +1,18 @@
+x = 10
+y = 2
+s = "ciao"
+#print(x / y)
+#print(s + x)
+print(s*y)
+
+pi = 3.14159265359
+formatted_pi1 = "the value of pi is {:.2f}".format(pi)
+formatted_pi2 = f"The value of pi is {pi:.3f}"
+print(formatted_pi1)
+print(formatted_pi2)
+
+print("Hello\nWorld!")
+print("Name:\tAlice")
+print("C:\\Users\\Alice\\Documents")
+print('It\'s a sunny day')
+print("She said, \"Hello\".")
