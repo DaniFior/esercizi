5e53ac579967fef02ccb05e98a7255c742d824d0
DaniFior <fioravantidaniele@icloud.com>
2024-04-17 22:32:31 +0200

    update esercizi lezione2.py

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -7,3 +7,31 @@ print("Hello world!")
 name: str = "Mario"
 message: str = "Hello {} do you want otlearn some python together?".format(name)
 print (message)
+
+#DA QUI FATTO A CASA
+
+#esercizio 2-3
+name = "EriC"
+print ("Hello {}, can i teach you some python?".format(name))
+
+#esercizio 2-4
+print(str.lower(name))
+print(str.upper(name))
+print(str.title(name))
+
+#esercizio 2-5 e 2-6
+
+author_name = "Steve Jobs"
+quote = "Stay hungry, stay foolish"
+
+print(f"Once {author_name} said: \"{quote}\"")
+
+#esercizio 2-8
+
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
+
+#esercizio 3-1 
+
+names = ["Marco", "Giuseppe", "Francesco"]
+print(names[1:])
