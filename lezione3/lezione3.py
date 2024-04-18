4c3aee2734a5fe29b6671b2713b7e2c497c927c5
DaniFior <fioravantidaniele@icloud.com>
2024-04-18 13:37:22 +0200

    update

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -4,3 +4,9 @@
 
 leonardo = {"first_name" : "Leonardo", "last_name" : "Marianella", "age" : 20, "city" : "Rome"}
 print(leonardo["first_name"], leonardo["last_name"], leonardo["age"], leonardo["city"])
+
+#esercizio 6-2 
+
+numbers = {"Valerio" : 10, "Leonardo" : 5, "Daniele" : 69}
+
+print(numbers("Valerio"))
