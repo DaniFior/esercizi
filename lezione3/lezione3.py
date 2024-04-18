714a9407573079131c8a53761d22dadf7abf73df
DaniFior <fioravantidaniele@icloud.com>
2024-04-18 13:25:57 +0200

    upload file lezione3.py

---
+++ lezione3/lezione3.py
@@ -0,0 +1,6 @@
+#ESERCIZIO 6-1 .. 6-12
+
+#esercizio 6-1
+
+leonardo = {"first_name" : "Leonardo", "last_name" : "Marianella", "age" : 20, "city" : "Rome"}
+print(leonardo["first_name"], leonardo["last_name"], leonardo["age"], leonardo["city"])
