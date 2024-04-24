9b44f295198891c80c953c83ae598a21e3f3f36c
DaniFior <fioravantidaniele@icloud.com>
2024-04-24 12:56:51 +0200

    upload correzione lezione 24 aprile

--- lezione4/bubble_sort.py
+++ lezione4/bubble_sort.py
@@ -1,5 +1,22 @@
 def bubble_sort(A : list) -> list :
     for i in range(len(A)):
+        for j in range(len(A)-i-1):
+            if A[j] > A[j+1]:
+                temp : int = A[j]
+                A[j] = A[j+1]
+                A[j+1] = temp
+    return A
+
+A : list = []
+for i in range(1,1001):
+    A.append(i)
+bubble_sort(A)
+print(A)
+
+"""
+
+def bubble_sort(A : list) -> list :
+    for i in range(len(A)):
         for j in range(len(A)-1):
             if A[j] > A[j+1]:
                 temp : int = A[j]
@@ -12,3 +29,5 @@ for i in range(1,1001):
     A.append(i)
 bubble_sort(A)
 print(A)
+
+"""
