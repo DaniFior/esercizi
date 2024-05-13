9e4cad0f72acfc6112c86c0891d193a0be781471
DaniFior <fioravantidaniele@icloud.com>
2024-05-13 12:12:51 +0200

    sistemazione

---
+++ zoopark/zoo.py
@@ -0,0 +1,11 @@
+# Fence avra self.area, self.temp, self.habitat E SELF.ANIMAL. Scrivere una funziona che calcola l'area occupata: prende lista animali, 
+#           la scorre e si salva tutte le area, restituendo il valore disponibile
+# Zookeeper avra self.name, self.surname, self.id. ATTENZIONE al fatto che possono pulire i recinti, nutrire animali e altri compiti
+# FUNZIONALITA:
+# Funzione: 
+    # add_animal (va messa nella classe zookeeper), verifica prima disponibilità e verifica se dimensione abbastanza grande per contenerlo
+    #       verificare anche se l'habitat coincide con quello preferito e aggiornare l'area disponibile    
+    # feed (va messa nella classe zookeeper), attenzione alla crescita dell'area
+    # clean (va messa nella classe zookeeper)
+    # describe_zoo (va messa nella classe zoo), devono tornare tutti i valori dei guardini e delle fence
+
