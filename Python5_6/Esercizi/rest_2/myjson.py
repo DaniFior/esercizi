c9166dac720599f078f9c491cf155a74e6a1a5d2
DaniFior <fioravantidaniele@icloud.com>
2024-09-30 12:34:25 +0200

    up

---
+++ Python5_6/Esercizi/rest_2/myjson.py
@@ -0,0 +1,17 @@
+import json
+
+
+
+def JsonSerialize(data, sFile):
+    if type(dData) is not dict:
+        return False
+    try:
+        with open(sFile, "w") as write_file:
+            json.dump(data, write_file,indent=4)
+        return True
+    except:
+        return False
+
+def JsonDeserialize(sFile):
+    with open(sFile, "r") as read_file:
+        return json.load(read_file)
